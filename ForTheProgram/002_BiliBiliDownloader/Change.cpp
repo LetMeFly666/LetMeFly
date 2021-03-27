3985bc317dcd4938804dff66e9facfa1b4a1280a
LetMeFly666 <814114971@qq.com>
2021-03-27 15:53:44 +0800

    sleep 500ms

--- ForTheProgram/002_BiliBiliDownloader/Change.cpp
+++ ForTheProgram/002_BiliBiliDownloader/Change.cpp
@@ -1,6 +1,7 @@
 #include<Windows.h>
 int main()
 {
+	Sleep(500);
 	system("del BiliBiliDownloader.exe");
 	system("ren version1.exe BiliBiliDownloader.exe");
 	system("start BiliBiliDownloader.exe");
